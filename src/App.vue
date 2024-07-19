10deef0b9639ebeaf248df449b4f1d7ba5fc8be0
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-07-19 16:49:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <CalendarCard v-for="(month, index) in 12" :key="index" :month="index" :year="currentYear" />
+  </div>
+</template>
+
+<script>
+import CalendarCard from './components/CalendarCard.vue';
+
+export default {
+  components: {
+    CalendarCard,
+  },
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style>
+/* Tambahkan gaya tambahan jika diperlukan */
+</style>
---
+++ src/components/CalendarCard.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="py-4 px-5 border rounded-lg ">
+        <h2 class="text-xl font-semibold mb-4">{{ monthName }} {{ year }}</h2>
+        <div class="grid grid-cols-7 gap-2">
+            <div v-for="day in daysOfWeek" :key="day" :class="day === 'Sun' ? '!text-red-500' : ''" class="font-medium mb-2 text-neutral-500">{{ day }}</div>
+            <div v-for="day in blankDays" :key="day" class="flex justify-center items-center"><span class="rounded-full w-3 h-3 bg-neutral-100"></span></div>
+            <div v-for="day in daysInMonth" :key="day" :class="[dayClasses(day), 'text-center']">
+                {{ day }}
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        month: {
+            type: Number,
+            required: true,
+        },
+        year: {
+            type: Number,
+            required: true,
+        },
+    },
+    computed: {
+        daysOfWeek() {
+            return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        },
+        monthName() {
+            return new Date(this.year, this.month).toLocaleString("default", { month: "long" });
+        },
+        daysInMonth() {
+            const days = new Date(this.year, this.month + 1, 0).getDate();
+            return Array.from({ length: days }, (_, i) => i + 1);
+        },
+        blankDays() {
+            const firstDay = new Date(this.year, this.month, 1).getDay();
+            console.log(firstDay);
+            return Array.from({ length: firstDay }, (_, i) => i);
+        },
+    },
+    methods: {
+        dayClasses(day) {
+            const date = new Date(this.year, this.month, day);
+            const isSunday = date.getDay() === 0;
+            return {
+                "text-red-500": isSunday,
+            };
+        },
+    },
+};
+</script>
+  
+<style scoped>
+/* Tambahkan gaya tambahan jika diperlukan */
+</style>
+  
